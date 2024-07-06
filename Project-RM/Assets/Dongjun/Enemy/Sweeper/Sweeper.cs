2949bbc775c9a7059b02b53696d8f76b960346fb
Ozinger <chocosdj@naver.com>
2024-07-06 15:33:40 +0900

    어쌔신 애니메이터 파라미터 수정 / 오브메이지 애니메이터 잔버그 수정 / 창술사 구현 완료

---
+++ Project-RM/Assets/Dongjun/Enemy/Sweeper/Sweeper.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+
+public class Sweeper : MonoBehaviour
+{
+    private EnemyController enemyController;
+
+    private GameObject attack1Collider;
+    private GameObject attack2Collider;
+    private GameObject longAttackCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyController = GetComponent<EnemyController>();
+        attack1Collider = transform.Find("attack1Collider").gameObject;
+        attack1Collider.SetActive(false);
+        attack2Collider = transform.Find("attack2Collider").gameObject;
+        attack2Collider.SetActive(false);
+        longAttackCollider = transform.Find("longAttackCollider").gameObject;
+        longAttackCollider.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Attack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            LongAttackPrepare();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            LongAttack();
+        }
+    }
+
+
+    void Attack()
+    {
+        attack1Collider.SetActive(false);
+        attack2Collider.SetActive(false);
+        enemyController.velocity = 0;
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 direction = (player.position - transform.position).normalized;
+        if (direction.x < 0f)
+        {
+
+            transform.localScale = new Vector3(-3, 3, 0);
+        }
+        else
+        {
+            transform.localScale = new Vector3(3, 3, 0);
+        }
+
+        if (enemyController.attackStack % 2 == 1) 
+        { 
+            enemyController.animator.SetTrigger("attack1"); 
+            attack1Collider.SetActive(true); 
+            attack2Collider.SetActive(false); 
+        }
+        if (enemyController.attackStack % 2 == 0) 
+        { 
+            enemyController.animator.SetTrigger("attack2"); 
+            attack2Collider.SetActive(true); 
+            attack1Collider.SetActive(false); 
+        }
+    }
+
+    void LongAttackPrepare()
+    {
+        longAttackCollider.SetActive(false);
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 direction = (player.position - transform.position).normalized;
+        if (direction.x < 0f)
+        {
+            transform.localScale = new Vector3(-3, 3, 0);
+        }
+        else
+        {
+            transform.localScale = new Vector3(3, 3, 0);
+        }
+        enemyController.isCharging = true;
+        enemyController.animator.SetBool("isCharging", true);
+    }
+
+    void LongAttack()
+    {
+        longAttackCollider.SetActive(true);
+        enemyController.animator.SetTrigger("longAttack");
+    }
+
+    public void attackStackPlus()
+    {
+        enemyController.attackStack += 1;
+    }
+
+    public void back()
+    {
+        enemyController.isCharging = false;
+        enemyController.animator.SetBool("isCharging", false);
+        attack1Collider.SetActive(false);
+        attack2Collider.SetActive(false);
+        longAttackCollider.SetActive(false);
+        enemyController.animator.SetTrigger("back");
+    }
+}
