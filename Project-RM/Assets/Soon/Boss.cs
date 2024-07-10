15ff72cb4fa26275c672dc72738681512a26d35c
tndmstla <jineung78@gmail.com>
2024-07-10 21:26:25 +0900

    보스 애니메이션 제작2

---
+++ Project-RM/Assets/Soon/Boss.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator animator;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Attack1();
+        }
+
+    }
+
+
+    void Jump()
+    {
+        animator.SetBool("IsJump", true);
+    }
+
+    void Attack1()
+    {
+        animator.SetTrigger("Attack2");
+    }
+}
