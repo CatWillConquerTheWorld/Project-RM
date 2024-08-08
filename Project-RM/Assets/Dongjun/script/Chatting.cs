a4a039429a5789ffc18e47a5caa3f482029cfff2
Ozinger <chocosdj@naver.com>
2024-08-08 19:24:27 +0900

    기능 추가 : 대사 기능 구현

--- Project-RM/Assets/Dongjun/script/Chatting.cs
+++ Project-RM/Assets/Dongjun/script/Chatting.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Chatting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject chatter;
+    public GameObject innerText;
+    public AudioSource audioSource;
+
+
+    private SpriteRenderer chatterSpriteRenderer;
+    private TextMeshPro innerTextTMPro;
+
+    private WaitForSeconds chatDelay;
+
+
     void Start()
     {
-        
+        chatter.SetActive(false);
+        innerText.SetActive(false);
+        chatterSpriteRenderer = chatter.GetComponent<SpriteRenderer>();
+        innerTextTMPro = innerText.GetComponent<TextMeshPro>();
+
+        chatDelay = new WaitForSeconds(0.075f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            chatter.SetActive(true);
+            innerText.SetActive(true);
+            StartCoroutine(Chat(4, 0.75f, "¿ï¶ö¶ó ¼¼¼Ç"));
+        }
+    }
+
+
+    IEnumerator Chat(float chatSizeX, float chatSizeY, string text)
+    {
+        innerTextTMPro.text = "";
+        chatterSpriteRenderer.size = new Vector2 (chatSizeX, chatSizeY);
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] != ' ')
+            {
+                audioSource.Play();
+            }
+            innerTextTMPro.text += text[i];
+            yield return chatDelay;
+        }
     }
 }
